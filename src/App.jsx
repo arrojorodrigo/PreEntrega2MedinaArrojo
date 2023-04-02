c8dbe1d7017fd1b7d2d644ce959775cb7509131e
Rodrigo Medina Arrojo <marrojorodrigo@gmail.com>
2023-04-02 00:36:43 -0300

    BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import axios from "axios";
 import ResponsiveAppBar from "./components/ResponsiveAppBar/index";
 import ProductList from "./components/ProductList"
 import Container from '@mui/material/Container';
+import { Route, Routes } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
+import Home from './components/Home';
 
 function App() {  
   const [products, setProducts] = useState([])
@@ -18,9 +21,15 @@ function App() {
 
   return (
     <div>
+      <Routes>
+        <Route path="/" element= { <Navigate to="/home"/> } />
+        <Route path="/home" element= { <Home /> } />
+        <Route path="/products" element= { <ProductList products={products} /> } />
+      </Routes>
+      {/* {products.map(product) => (<h3>{product.title}</h3>)} */}
       <ResponsiveAppBar/>
       <Container sx={{ mt: 5 }}>
-      <ProductList products={products}/>
+      {/* <ProductList products={products}/> */}
       </Container>
     </div>
   );
